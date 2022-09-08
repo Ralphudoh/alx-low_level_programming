c0d00c643b1532cbdceb796f03f383174a2d01f6
Ralphudoh <udohraphael30@gmail.com>
2022-09-08 16:38:56 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
-/*
- * File: 4-puts.c
- * Auth: Raphael Udoh 
- */
-
 #include <stdio.h>
 
 /**
- * main -print the string in the put function
+ * main - print the string in the put function
  *
  * Descriptiong: using the main function 
  * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
- * */
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
