25149e163783a39d20cdceb416957866e3821e71
Ralphudoh <udohraphael30@gmail.com>
2022-09-08 16:07:59 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 /*
  * File: 101-quote.c
- * Auth: Raphael Udoh 
+ * Auth: Raphael Udoh
  */
 
- #include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -12,9 +12,11 @@
  * Return: Always 1.
  */
 int main(void)
+
 {
 	write(2,
-  	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-              59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+      	      59);
+
 	return (1);
 }
