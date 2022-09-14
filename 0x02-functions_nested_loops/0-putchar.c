2e87f1bfcf82b81540b22c7ac1156313247f0f6f
Ralphudoh <udohraphael30@gmail.com>
2022-09-13 19:46:50 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,16 @@
  *Return: Always 0
  */
 
-int 'main'(void)
+int main(void)
 
-{	_putchar ('_');
-	_putchar ('P');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
-	_putchar ('/n');
-
+{	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('/n');
 	return (0);
 }
