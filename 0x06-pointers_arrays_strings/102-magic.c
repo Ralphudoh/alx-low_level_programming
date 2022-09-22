52e97b8bc95ff4e0457c2e4df239c7c09b21f41a
Ralphudoh <udohraphael30@gmail.com>
2022-09-22 12:54:01 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - check the code magic c file.
+ * Return: Always 0.
+ */
 int main(void)
-
 {
 	int n;
 	int a[5];
@@ -17,8 +20,9 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
+
 	 *(p + 5) = 98;
 	 /* ...so that this prints 98\n */
-	 printf("a[2] = %d\n", a[2]);
-	 return (0);
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
