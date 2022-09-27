f20502f32503aa72f64ea72437ad679abed88137
Ralphudoh <udohraphael30@gmail.com>
2022-09-27 03:14:10 -0700

    Task 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,13 +18,12 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
-		{
 			if (s[i] == accept[j])\
 			{
 				result++;
 				break;
 			}
-		}
+	}
 		if (s[j] == '\0')
 			return (result);
 	}
