fb6873396100b2f31e94b33b8da7a3480062094e
Ralphudoh <udohraphael30@gmail.com>
2023-01-15 09:39:45 -0800

    Task 0

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: string
+*
+*Return: int (size of string)
+*/
+int _strlen(char *s)
+{
+	int letter = 0;
+
+	while (s[letter] != '\0')
+	{
+	letter++;
+	}
+	return (letter);
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - print string with array
+* @str: string
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i[str] != '\0')
+	{
+	_putchar (str[i]);
+	i++;
+	}
+	_putchar ('\n');
+}
